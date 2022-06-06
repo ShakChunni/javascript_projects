ecab75c274b9541b1c01b86179fcf831e3e3baf2
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2022-06-06 21:04:58 +0600

    first commit

---
+++ counter/counter.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn){
+    //console.log();
+    btn.addEventListener("click", function(e){
+        const styles = (e.currentTarget.classList);
+        if(styles.contains('increase')){
+            count++;
+        }
+        else if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('reset')){
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
---
+++ reviews/reviews.js
@@ -0,0 +1,74 @@
+//local reviews
+const reviews = [
+    {
+        id: 1,
+        Name:"MD Fuad Hasan",
+        job: "Blockchain specialist aka NFT bros😎",
+        img: "./20220602_154210.jpg",
+        text:"Ami very angry man, Tumar website TRASH. now buy my NFT 🤑",
+    },
+    {
+        id: 2,
+        Name:"Ahmed Awsaf Chowdhory",
+        job: "Ei ami cool chele!😎",
+        img: "./FB_IMG_1605446979630.jpg",
+        text:"Egula website diye ki hobe? amar sathe thako meye potnao shikhabo 😁",
+    },
+    {
+        id: 3,
+        Name:"Shoeb Islam Hamim",
+        job: "Everything specialist🐐",
+        img: "./20220516_151534.jpg",
+        text:"Thumbs up! 👍",
+    },
+];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randoBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  // show next person
+  nextBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener("click", function () {
+    console.log("hello");
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
